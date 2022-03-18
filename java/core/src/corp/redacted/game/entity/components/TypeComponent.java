59b225891c0b9716c450a942cec1c120688b1471
Manonzlamaguerite <m.girard42@hotmail.com>
2022-03-18 21:21:07 +0100

    feat : ajout d'un paramètre pour différencier les 2 bateaux.

--- java/core/src/corp/redacted/game/entity/components/TypeComponent.java
+++ java/core/src/corp/redacted/game/entity/components/TypeComponent.java
@@ -7,9 +7,9 @@ import com.badlogic.gdx.utils.Pool.Poolable;
 * Stocke le type de l'entité
 */
 public class TypeComponent implements Component, Poolable {
- public static final int BATEAU = 0;
- public static final int MARCHANDISE = 1;
- public static final int MER = 3;
+ public static final int BATEAU_A = 1;
+ public static final int BATEAU_B = 2;
+ public static final int MARCHANDISE = 3;
  public static final int BOULET = 4;
  public static final int OTHER = 5;
 
@@ -18,6 +18,11 @@ public class TypeComponent implements Component, Poolable {
  @Override
  public void reset(){
    type = OTHER;
+ }
+
+ @Override
+ public String toString(){
+   return ""+type;
  }
 
 }
