c49cfd2ee7c451c9aeaf763331441b21161ee517
Manonzlamaguerite <m.girard42@hotmail.com>
2022-03-18 21:22:00 +0100

    feat : ajout du système de collision

---
+++ java/core/src/corp/redacted/game/entity/systems/CollisionSystem.java
@@ -0,0 +1,107 @@
+package corp.redacted.game.entity.systems;
+
+import corp.redacted.game.entity.components.CollisionComponent;
+import corp.redacted.game.entity.components.StatComponent;
+import corp.redacted.game.entity.components.TypeComponent;
+import corp.redacted.game.entity.components.BodyComponent;
+import corp.redacted.game.IConfig;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.math.MathUtils;
+
+
+public class CollisionSystem extends IteratingSystem{
+  ComponentMapper<CollisionComponent> colM;
+  ComponentMapper<StatComponent> boatM;
+  ComponentMapper<BodyComponent> bodyMap;
+
+	public CollisionSystem() {
+		super(Family.all(CollisionComponent.class).get());
+		this.colM = ComponentMapper.getFor(CollisionComponent.class);
+		this.boatM = ComponentMapper.getFor(StatComponent.class);
+    this.bodyMap = ComponentMapper.getFor(BodyComponent.class);
+	}
+
+  @Override
+  protected void processEntity(Entity entite, float temps){
+    CollisionComponent colC = colM.get(entite); //On recupere une collision
+    BodyComponent bodyC = bodyMap.get(entite);
+    Entity entiteEnCollision = colC.collisionEntite; //On recupere l'entite en collision
+
+    TypeComponent typeEntite = entite.getComponent(TypeComponent.class); // On recupere son type
+
+    /*Consernant les collisions des joueurs (bateau)*/
+    if(typeEntite.type == TypeComponent.BATEAU_A){
+      StatComponent boat = boatM.get(entite);
+
+      if(entiteEnCollision != null){
+        TypeComponent typeEnCollsion = entiteEnCollision.getComponent(TypeComponent.class);
+        if(typeEnCollsion != null){
+          switch(typeEnCollsion.type){
+            case TypeComponent.BATEAU_B :
+            //On applique une force sur le bateau.
+            bodyC.body.setLinearVelocity(MathUtils.lerp(bodyC.body.getLinearVelocity().x, 10, 3f),MathUtils.lerp(bodyC.body.getLinearVelocity().y, 10, 3f));
+            System.out.println("cc"+bodyC.body.getLinearVelocity().x);
+
+            boat.barreVie -= IConfig.DEGAT_B_B; //Degat de la collision
+            System.out.println(boat.barreVie);
+
+            colC.collisionEntite = null;
+            break;
+            case TypeComponent.MARCHANDISE:
+            //On recupere la marchandise
+            //La marchandise disparait de la map
+            // Mise à jour des scores
+            // Manche finie
+            break;
+            case TypeComponent.BOULET:
+            //Mise à jour de la bar de vie
+            //Verif si pas de morts
+            default:
+            break;
+          }
+        }
+      }
+    }
+
+    /*Consernant les collisions des joueurs (bateau)*/
+    else if(typeEntite.type == TypeComponent.BATEAU_B){
+      StatComponent boat = boatM.get(entite);
+
+      if(entiteEnCollision != null){
+        TypeComponent typeEnCollsion = entiteEnCollision.getComponent(TypeComponent.class);
+        if(typeEnCollsion != null){
+          switch(typeEnCollsion.type){
+            case TypeComponent.BATEAU_A:
+            //On applique une force sur le bateau.
+            bodyC.body.setLinearVelocity(MathUtils.lerp(bodyC.body.getLinearVelocity().x, 10, 3f),MathUtils.lerp(bodyC.body.getLinearVelocity().y, 10, 3f));
+
+            boat.barreVie -= IConfig.DEGAT_B_B; //Degat de la collision
+            colC.collisionEntite = null;
+            break;
+            case TypeComponent.MARCHANDISE:
+            //On recupere la marchandise
+            //La marchandise disparait de la map
+            // Mise à jour des scores
+            // Manche finie
+            break;
+            case TypeComponent.BOULET:
+            //Mise à jour de la bar de vie
+            //Verif si pas de morts
+            default:
+            break;
+          }
+        }
+      }
+    }
+
+
+
+  }
+
+
+}
