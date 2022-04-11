7574c59a80ff14bc56735a066051d5e6d5dee4e0
maxhr <maxhr.0x90@gmail.com>
2022-04-11 14:35:43 +0200

    feat: Generateur du modeèle de la mer

--- java/core/src/corp/redacted/game/ModelGenerator.java
+++ java/core/src/corp/redacted/game/ModelGenerator.java
@@ -1,42 +1,32 @@
 package corp.redacted.game;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.model.Node;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import corp.redacted.game.model.SandShapeBuilder;
 
 /**
  * Méthodes de génération des diffenrents modèles du jeu
  */
 public class ModelGenerator {
-    private static final float BOAT_DEPTH = 4f;
+    public static Model seaModel(
+            float viewWidth, float depth, float alt0Width, float alt0Height, int stepNb, float stiffness
+    ){
+        ModelBuilder builder = new ModelBuilder();
 
-    /**
-     *
-     * @param width Largeur du modele
-     * @param height Profondeur du modèle
-     * @param color Couleur du modèle
-     * @return Modèle 3D de bateau (version debug)
-     */
-    public static Model debugBoatModel(float width, float height, Color color){
-        ModelBuilder modelBuilder = new ModelBuilder();
-        return modelBuilder.createBox(width, height, BOAT_DEPTH,
-                new Material(ColorAttribute.createDiffuse(color)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-    }
+        builder.begin();
 
-    /**
-     *
-     * @param radius Rayon du boulet
-     * @return Modèle 3D de boulet de canon (version debug)
-     */
-    public static Model debugCanonBallModel(float radius){
-        ModelBuilder modelBuilder = new ModelBuilder();
-        return modelBuilder.createSphere(radius, radius, radius, 50, 50,
-                new Material(ColorAttribute.createDiffuse(Color.GRAY)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        builder.node("sand", sandModel(viewWidth, depth, alt0Width, alt0Height, stepNb, stiffness));
+        builder.node("water", waterModel(alt0Width, alt0Height));
+
+        return builder.end();
     }
 
     /**
@@ -45,16 +35,46 @@ public class ModelGenerator {
      * @param height Profondeur du modèle
      * @return Modèle 3D de la mer
      */
-    public static Model debugSeaModel(float width, float height){
+    public static Model waterModel(float width, float height){
         ModelBuilder modelBuilder = new ModelBuilder();
-        return modelBuilder.createRect(
-                width/2, -height/2, 0,
+        Model water = modelBuilder.createRect(
                 -width/2, -height/2, 0,
-                -width/2, height/2, 0,
+                width/2, -height/2, 0,
                 width/2, height/2, 0,
+                -width/2, height/2, 0,
                 0, 1, 0,
-                new Material(ColorAttribute.createDiffuse(Color.SKY)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal
+                new Material(
+                        ColorAttribute.createDiffuse(Color.SKY),
+                        new BlendingAttribute(0.5f)
+                ),
+                Usage.Position | Usage.Normal
         );
+
+        water.nodes.get(0).id = "water";
+
+        return water;
+    }
+
+    public static Model sandModel(
+            float viewWidth, float depth, float alt0Width, float alt0Height, int stepNb, float stiffness
+    ){
+        MeshPartBuilder meshBuilder;
+        ModelBuilder builder = new ModelBuilder();
+
+        builder.begin();
+
+        Node node = builder.node();
+        node.id = "sand";
+
+        meshBuilder = builder.part("sand", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(
+                ColorAttribute.createDiffuse(Color.GOLDENROD)
+        ));
+
+        SandShapeBuilder sandBuilder = new SandShapeBuilder(
+                meshBuilder, viewWidth, depth, alt0Width, alt0Height, stepNb, stiffness
+        );
+        sandBuilder.build();
+
+        return builder.end();
     }
 }
